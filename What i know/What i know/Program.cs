4c8bf477921c2b30d6d3b920e80c078e8821b07c
Zane-2244 <49199705+Zane-2244@users.noreply.github.com>
2019-06-05 11:48:56 +0200

    Add files via upload

---
+++ What i know/What i know/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace What_i_know
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+         byte b = 100;
+            int i = b;
+            Console.WriteLine(i);
+            var numbers = new List<int>();
+            string s = "1234";
+            int u = Convert.ToInt32(s);
+            Console.WriteLine(u);
+            while (true)
+            {
+                Console.WriteLine("Enter a name: ");
+                var name = Console.ReadLine();
+                Console.WriteLine("@Echo: "+ name);
+
+                if (string.IsNullOrWhiteSpace(" "))
+                {
+                    Console.WriteLine("there is no name!!");
+                }
+                else;
+                {
+                    Console.WriteLine("Hello there nice to meet you");
+                }
+
+            }
+           
+
+
+
+        }
+    }
+}
